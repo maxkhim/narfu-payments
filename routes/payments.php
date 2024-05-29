55405e108d8e9a5ef4a35fe2e9ad9ad6f652c82c
Maxim Khimusov <maxkhim@yandex.ru>
2024-05-29 12:52:34 +0300

    Настройки троттлинга

--- routes/payments.php
+++ routes/payments.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth', 'verified'/*, AdminMiddleware::class*/])->group(function () {
+Route::middleware(['web', 'auth', 'verified'/*, AdminMiddleware::class*/, 'throttle:60,1'])->group(function () {
 
     Route::group(['prefix' => '/narfu/payments'], function () {
         Route::get('/', [\Narfu\Payments\Http\Controllers\PaymentsController::class, 'index'])
@@ -12,7 +12,7 @@ Route::middleware(['web', 'auth', 'verified'/*, AdminMiddleware::class*/])->grou
 
 });
 
-Route::middleware(['web', 'guest'])->group(function () {
+Route::middleware(['web', 'guest', 'throttle:60,1'])->group(function () {
     Route::group(['prefix' => '/narfu/payments-guest'], function () {
         Route::get('/', [\Narfu\Payments\Http\Controllers\PaymentsController::class, 'indexGuest'])
             ->name('narfu.payments-guest');
